1365213519d9c32e131c02a1686d0ea6e9f67053
Cesar Ignacio <aquinocesarignacio@gmail.com>
2021-02-01 19:56:39 -0300

    Suma dos numero

---
+++ Holamundo.py
@@ -0,0 +1,22 @@
+
+#mi primer hola mundo 
+print ("Hola mundo")
+
+num1= int (input ("ingrese primer numero:"))
+num2= int (input("ingrese segundo numero:"))
+
+suma= num1+num2
+
+if suma>18:
+
+    print ("EL SUMA EL MAYOR A 100")
+
+else:
+
+    print ("LA SUMA ES MENOR QUE 100")
+
+
+print ("SUMA:",suma)
+
+
+
